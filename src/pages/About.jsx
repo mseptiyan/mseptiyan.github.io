52fb05fbbaba4f5991ede3f9dbceffcd21d914c1
mseptiyan <johangustix@gmail.com>
2026-05-20 16:20:58 +0700

    fix: component name case

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,7 @@ import AboutHero from '../components/about/AboutHero';
 import Timeline from '../components/about/Timeline';
 import Credentials from '../components/about/Credentials';
 import Roadshow from '../components/about/Roadshow';
-import InTheWild from '../components/about/InTheWild';
+import IntheWild from '../components/about/IntheWild'
 
 export default function About() {
   return (
